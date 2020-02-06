103e30d292a3671ce9d33cbdf6d9ce5d3ed42dc5
Timothy Kay <timkay@not.com>
2020-02-06 15:30:26 -0800

    added image paste

--- index.js
+++ index.js
@@ -44,24 +44,72 @@ function displayImageArray(w, h, data) {
 // const imgurl = 'pasted_7wGRfLbLPk.jpg';
 // const imgurl = 'pasted_MYGkVdO8nK.jpg';
 const imgurl = 'pasted_NQMQZtdU0t.jpg';
-$('img').attr('src', imgurl);
+$('#img').attr('src', imgurl);
 
-loadImageArray(imgurl, (w, h, data) => {
-    console.log('width, height', w, h);
-    let hist = {};
-    let k = 0;
-    for (let i = 0; i < h; i++) {
-        for (let j = 0; j < w; j++) {
-            let value = (data[k + 0] << 16) | (data[k + 1] << 8) | (data[k + 2] << 0);
-            hist[value] = (hist[value] || 0) + 1;
-            k += 4;
+function go(url) {
+    loadImageArray(url, (w, h, data) => {
+        let hist = {};
+        let k = 0;
+        for (let i = 0; i < h; i++) {
+            for (let j = 0; j < w; j++) {
+                let value = (data[k + 0] << 16) | (data[k + 1] << 8) | (data[k + 2] << 0);
+                hist[value] = (hist[value] || 0) + 1;
+                k += 4;
+            }
+        }
+        $('#histogram').html('');
+        let values = Object.keys(hist).filter(value => hist[value] > 20);
+        values.forEach(value => {
+            const r = value >> 16;
+            const g = (value >> 8) & 255;
+            const b = value & 255;
+            $('#histogram').append(`<div class="swatch" style="background: rgb(${[r, g, b]})">${hist[value]}</div>`);
+        })
+    });
+}
+
+$('body').on('paste', function (event) {
+    let items = (event.clipboardData || event.originalEvent.clipboardData).items;
+    for (let index in items) {
+        let item = items[index];
+        if (item.kind === 'file') {
+            let blob = item.getAsFile();
+            let reader = new FileReader();
+            reader.onload = (event) => {
+                let url = event.target.result;
+                $('#img').attr('src', url);
+                go(url);
+            };
+            reader.readAsDataURL(blob);
         }
     }
-    let values = Object.keys(hist).filter(value => hist[value] > 20);
-    values.forEach(value => {
-        const r = value >> 16;
-        const g = (value >> 8) & 255;
-        const b = value & 255;
-        $('body').append(`<div class="swatch" style="background: rgb(${[r, g, b]})">${hist[value]}</div>`);
-    })
 });
+
+go(imgurl);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
