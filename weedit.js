46294aa03d1f347e8b156bb5a2bf07c6ccc0dd83
Tim Kay <timkay@not.com>
2021-03-16 23:47:22 -0700

    Delete weedit.js

--- weedit.js
+++
@@ -1,7 +0,0 @@
-
-if (location === parent.location) {
-    $('body').append('<img title="edit in webEdit" id="popin" height="16" src="//webedit.com/favicon.ico" style="cursor: pointer; position: fixed; top: 4px; right: 4px;"/>');
-    $('#popin').click(event => {
-        location.href = '?run=edit';
-    });
-}
